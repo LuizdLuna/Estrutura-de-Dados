08c097f280300521f3e83c5b43e98722a2879d76
LuizdLuna <159401389+LuizdLuna@users.noreply.github.com>
2025-02-16 04:11:06 +0000

    Exercicio 2

---
+++ Exercicio2.java
@@ -0,0 +1,22 @@
+// #Exercício 2: Estruturas de Decisão (faça esse em  Java)
+//Descrição: Crie um programa que solicite ao usuário uma nota (0-10) e exiba uma mensagem indicando se ele foi aprovado (nota >= 7), está de recuperação (nota entre 5 e 6.9) ou foi reprovado (nota < 5).
+
+import java.util.*;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite a nota do aluno (entre 0 e 10): ");
+        double nota = input.nextDouble();
+
+        if (nota >= 7){
+            System.out.println("Aluno aprovado!!");
+        } else if (nota <= 6.9 && nota >= 5){
+            System.out.println("Aluno de recuperacao!!");
+        } else {
+            System.out.println("Aluno REPROVADO!!!");
+        }
+    }
+}
